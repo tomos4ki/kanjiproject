a4ae180c0afec26a675f36802f8212011d1cbcbe
tommos4ki <sarimu.medfai@gmail.com>
2025-02-24 23:07:29 +0100

    update

--- kanjiapp/models.py
+++ kanjiapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 from django.db import models
 
 class Kanji(models.Model):
-    character = models.CharField(max_length=1)
+    character = models.CharField(max_length=100000)
     english_translation = models.CharField(max_length=100)
     onyomi = models.CharField(max_length=100)
     kunyomi = models.CharField(max_length=100)
     stroke_count = models.IntegerField()
-    jlpt_level = models.IntegerField()
+    jlpt_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     example_sentence = models.TextField()
 
     def __str__(self):
--- kanjiapp/urls.py
+++ kanjiapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home', views.kanji_list, name='kanji_list'),
     path('', views.kanji_list, name='kanji_list'),
     path('kanji/<int:pk>/', views.kanji_detail, name='kanji_detail'),
     path('kanji/new/', views.kanji_create, name='kanji_create'),
